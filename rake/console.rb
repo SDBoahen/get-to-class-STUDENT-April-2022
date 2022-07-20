cfe975afc4d7742f8efaff8d13782f917053e3b4
SDBoahen <SDBoahen@gmail.com>
2022-07-20 18:15:43 -0400

    Clean Reset

--- rake/console.rb
+++ rake/console.rb
@@ -3,13 +3,7 @@ require_relative "../config/environment"
 
     ####  seeds.rb
 
-        scottay  = Student.new( 1   , "Scoootahy", "Westport", true)
-        uri      = Student.new( 2   , "Uri", "Morristown", true)
-        evan     = Student.new( 3   , "Evan", "Great Falls", true)
-        sakinah  = Student.new( 4   , "Sakinah", "Leesburg", true)
-        drew     = Student.new( 5   , "Drew Grey", "Princeton", true)
-        joey     = Student.new( 6   , "Joey<HA$HTAGZ>", "Newark", true)
-        johannes = Student.new( 777 , "Johannes", "Cabo Rojo", true)
+
 
     ####
 
